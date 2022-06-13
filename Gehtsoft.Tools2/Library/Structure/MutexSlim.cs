0c6c6b6ee4117643a7613c3d93f53f35f1b78ab1
Nikolay Gekht <nikolay.gekht@gehtsoftusa.com>
2022-06-13 13:53:50 -0400

    - add mutexslim

---
+++ Gehtsoft.Tools2/Library/Structure/MutexSlim.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Gehtsoft.Tools2.Structure
+{
+    public class MutexSlim : IDisposable
+    {
+        private SemaphoreSlim mSemaphore = new SemaphoreSlim(1, 1);
+        private int mConsequentAquiring = 0;
+        private ulong mAcquiringThreadId = 0;
+
+        private static ulong CurrentId
+        {
+            get
+            {
+                int? taskId = Task.CurrentId;
+
+                const ulong t1 = 0x100000000;
+                const ulong t2 = 0x200000000;
+                ulong m, r;
+
+                if (taskId != null)
+                {
+                    m = t1;
+                    r = ((ulong)taskId) & 0xffffffff;
+                }
+                else
+                {
+                    m = t2;
+                    r = ((ulong)Thread.CurrentThread.ManagedThreadId) & 0xffffffff;
+                }
+
+                return m | r;
+            }
+        }
+
+        public WaitHandle WaitHandle => mSemaphore.AvailableWaitHandle;
+
+        public MutexSlim()
+        {
+        }
+
+        public MutexSlim(bool owned)
+        {
+            if (owned)
+                Wait();
+        }
+
+        ~MutexSlim()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            mSemaphore.Dispose();
+        }
+
+        private class Unlocker : IDisposable
+        {
+            private readonly MutexSlim mMutex;
+
+            public Unlocker(MutexSlim mutex)
+            {
+                mMutex = mutex;
+            }
+
+            public void Dispose()
+            {
+                mMutex.Release();
+            }
+        }
+
+        private Unlocker mUnlocker = null;
+
+        private Unlocker SingleUnlocker => mUnlocker ?? (mUnlocker = new Unlocker(this));
+
+        public IDisposable Lock()
+        {
+            Wait();
+            return SingleUnlocker;
+        }
+
+        public async Task<IDisposable> LockAsync()
+        {
+            await WaitAsync();
+            return SingleUnlocker;
+        }
+
+        private static TimeSpan MaxTimeout = TimeSpan.FromMilliseconds(Int32.MaxValue);
+
+        public void Wait() => Wait(MaxTimeout, CancellationToken.None);
+
+        public bool Wait(int timeout) => Wait(TimeSpan.FromMilliseconds(timeout), CancellationToken.None);
+
+        public bool Wait(TimeSpan timeout) => Wait(timeout, CancellationToken.None);
+
+        public void Wait(CancellationToken token) => Wait(MaxTimeout, token);
+
+        public bool Wait(TimeSpan timeout, CancellationToken token)
+        {
+            ulong id = CurrentId;
+            if (mSemaphore.CurrentCount == 0 && mAcquiringThreadId == id)
+            {
+                mConsequentAquiring++;
+                return true;
+            }
+            bool rc = mSemaphore.Wait(timeout, token);
+            if (rc)
+                mAcquiringThreadId = id;
+            return rc;
+        }
+
+        public Task WaitAsync() => WaitAsync(MaxTimeout, CancellationToken.None);
+
+        public Task<bool> WaitAsync(int timeout) => WaitAsync(TimeSpan.FromMilliseconds(timeout), CancellationToken.None);
+
+        public Task<bool> WaitAsync(TimeSpan timeout) => WaitAsync(timeout, CancellationToken.None);
+
+        public Task WaitAsync(CancellationToken token) => WaitAsync(MaxTimeout, token);
+
+        public async Task<bool> WaitAsync(TimeSpan timeout, CancellationToken token)
+        {
+            ulong id = CurrentId;
+            if (mSemaphore.CurrentCount == 0 && mAcquiringThreadId == id)
+            {
+                mConsequentAquiring++;
+                return true;
+            }
+
+            bool rc = await mSemaphore.WaitAsync(timeout, token);
+            if (rc)
+                this.mAcquiringThreadId = CurrentId;
+            return rc;
+        }
+
+        public bool IsLocked => mSemaphore.CurrentCount == 0;
+
+        public bool IsLockedByMe => IsLocked && CurrentId == mAcquiringThreadId;
+
+        public void Release()
+        {
+            if (mConsequentAquiring > 0)
+                mConsequentAquiring--;
+            else
+            {
+                mAcquiringThreadId = 0;
+                mSemaphore.Release(1);
+            }
+        }
+    }
+}
---
+++ Gehtsoft.Tools2/Test/MutexSlimTest.cs
@@ -0,0 +1,157 @@
+﻿using FluentAssertions;
+using Gehtsoft.Tools2.Structure;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace Gehtsoft.Tools2.UnitTest
+{
+    public class MutexSlimTest
+    {
+        private readonly ITestOutputHelper mTestOutputHelper;
+
+        public MutexSlimTest(ITestOutputHelper testOutputHelper)
+        {
+            mTestOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void UnlockedOnCreation()
+        {
+            using var ms = new MutexSlim();
+            ms.IsLocked.Should().BeFalse();
+        }
+
+        [Fact]
+        public void LockFromNew()
+        {
+            using var ms = new MutexSlim();
+            ms.Wait(0).Should().BeTrue();
+            ms.IsLocked.Should().BeTrue();
+            ms.IsLockedByMe.Should().BeTrue();
+        }
+
+        [Fact]
+        public void CanEnterTheSameThreat()
+        {
+            using var ms = new MutexSlim();
+            ms.Wait(0).Should().BeTrue();
+            ms.Wait(0).Should().BeTrue();
+            ms.Release();
+            ms.IsLocked.Should().BeTrue();
+            ms.Release();
+            ms.IsLocked.Should().BeFalse();
+        }
+
+        [Fact]
+        public void UnlockedAfterRelease()
+        {
+            using var ms = new MutexSlim();
+            ms.Wait(0).Should().BeTrue();
+            ms.Release();
+            ms.IsLocked.Should().BeFalse();
+        }
+
+        [Fact]
+        public void LockAfterUnlock()
+        {
+            using var ms = new MutexSlim();
+            ms.Wait(0).Should().BeTrue();
+            ms.IsLocked.Should().BeTrue();
+            ms.Release();
+            ms.Wait(0).Should().BeTrue();
+            ms.IsLocked.Should().BeTrue();
+        }
+
+        [Fact]
+        public void CantEnterFromAnotherThreatWhileLocked()
+        {
+            using var ms = new MutexSlim();
+            bool? r = null;
+            
+            Thread t = new Thread(() => r = ms.Wait(0));
+            using (var l = ms.Lock())
+            {
+                t.Start();
+                t.Join();
+            }
+
+            r.Should().NotBeNull();
+            r.Should().BeFalse();
+        }
+
+        [Fact]
+        public void LockByLockObject()
+        {
+            using var ms = new MutexSlim();
+            using (var l = ms.Lock())
+                ms.IsLocked.Should().BeTrue();
+            ms.IsLocked.Should().BeFalse();
+        }
+
+        [Fact]
+        public async Task LockByLockObjectAsync()
+        {
+            using var ms = new MutexSlim();
+            using (var l = await ms.LockAsync())
+                ms.IsLocked.Should().BeTrue();
+            ms.IsLocked.Should().BeFalse();
+        }
+
+        [Fact]
+        public void CanWaitInAnotherThreat()
+        {
+            using var ms = new MutexSlim();
+            bool? r = null;
+
+            Thread t = new Thread(() => r = ms.Wait(10000));
+            using (var l = ms.Lock())
+                t.Start();
+
+            t.Join();
+
+            r.Should().NotBeNull();
+            r.Should().BeTrue();
+            ms.IsLocked.Should().BeTrue();
+            ms.IsLockedByMe.Should().BeFalse();
+        }
+
+        [Fact]
+        public void PerformanceTest()
+        {
+            var mutex = new Mutex();
+            var mutexSlim = new MutexSlim();
+
+            var sw = new Stopwatch();
+
+            sw.Reset();
+            sw.Start();
+            for (int i = 0; i < 50000; i++)
+            {
+                mutex.WaitOne();
+                mutex.ReleaseMutex();
+            }
+            sw.Stop();
+            var mutexTime = sw.Elapsed;
+
+            sw.Reset();
+            sw.Start();
+            for (int i = 0; i < 50000; i++)
+            {
+                mutexSlim.Wait();
+                mutexSlim.Release();
+            }
+            sw.Stop();
+            var mutexSlimTime = sw.Elapsed;
+
+            mTestOutputHelper.WriteLine("Mutex {0} Slim {1}", mutexTime, mutexSlimTime);
+
+            mutexSlimTime.Should().BeLessThan(mutexTime);
+        }
+    }
+}
